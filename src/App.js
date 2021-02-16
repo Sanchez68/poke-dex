10250710ec5c31db3a20ede2bb474d05fac74e7c
Tyshkevych <Dekstertop3@gmail.com>
2021-02-16 12:12:52 +0200

    vivivi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          VITYA PETYXds
+          Vivivivi
         </p>
         <a
           className="App-link"
