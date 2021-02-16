554d7c21537ec8d6f3393fbfd3cfdc666e26ba25
Tyshkevych <Dekstertop3@gmail.com>
2021-02-16 11:52:34 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          VITYA PETYX
+          VITYA PETYXd
         </p>
         <a
           className="App-link"
