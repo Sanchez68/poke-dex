2582ea27455973165e208dc51844ff027edc0364
Tyshkevych <Dekstertop3@gmail.com>
2021-02-20 22:10:24 +0200

    markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import HeaderContainer from "./components/Header/HeaderContainer";
+import BodyContainer from "./components/Body/BodyContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vivivivivi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    render() {
+
+        return (
+            <div className="App">
+                <div className='header'>
+                    <HeaderContainer/>
+                </div>
+                <div className='body'>
+                    <BodyContainer/>
+                </div>
+            </div>)
+    }
 }
 
 export default App;
